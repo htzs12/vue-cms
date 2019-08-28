72faf9ed3dec5bbc434de819d3efdf28bb7c280f
htzs12 <www.htzs@qq.com>
2019-08-28 15:46:45 +0800

    'update-1'

---
+++ src/components/tabbar/HomeContainer.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <div>
+    <!-- 这是轮播图 -->
+    <mt-swipe :auto="4000">
+      <mt-swipe-item>1</mt-swipe-item>
+      <mt-swipe-item>2</mt-swipe-item>
+      <mt-swipe-item>3</mt-swipe-item>
+    </mt-swipe>
+    <h1>啊啊啊</h1>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+  .mint-swipe {
+    height: 200px;
+
+    .mint-swipe-item {
+      &:nth-child(1) {
+      background: pink;
+    }
+    }
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,18 +2,26 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import app from './App.vue'
+import VueRouter from 'vue-router'
+import router from './router.js'
 import './lib/mui/css/mui.css'
 import './lib/mint/header/style.css'
+import './lib/mint/swipe/style.css'
 
-import { Header } from 'mint-ui'
+import {Header, Swipe, SwipeItem} from 'mint-ui'
 
 // Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
 Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
 
 /* eslint-disable no-new */
 
 new Vue({
   el: '#app',
-  render: c => c(app)
+  render: c => c(app),
+  router // 挂在路由对象到vue实例上
 })
